021f6776fd5c23285c90b39b13d74c1c5547c483
SushantVarshney21 <Sushantvars@gmail.com>
2025-01-27 19:06:52 +0530

    aa

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Configure CORS to allow only the specific origin
 const corsOptions = {
-  origin: "https://mv-readymade-invoicefrontend.vercel.app", // Frontend URL
+  origin: "*", // Frontend URL
   credentials: true, // Allow cookies and headers for authentication
 };
 
